69a42185f77c9c01e285334c6bce3a66081551c8
parkinsonlee <parkinsonlee02@gmail.com>
2022-03-27 14:30:17 -0400

    added basics

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ import java.util.ArrayList;
 public class Main {
     static ArrayList<Wiki> wikis = new ArrayList<>();
 
-    public static void main(String[] args) {//main method: creates and reads every file from deck directory and runs everytime to ensure deck directory is always there. Also runs ui.
+    public static void main(String[] args) {
     }
 }
