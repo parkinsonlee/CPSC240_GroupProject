1ac4884fab9c3d211d5c10bd368071b986774c05
parkinsonlee <parkinsonlee02@gmail.com>
2022-03-27 14:37:10 -0400

    added basics

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,49 @@
 package src;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
     static ArrayList<Wiki> wikis = new ArrayList<>();
 
     public static void main(String[] args) {
+        //lines 15-47 read files in wiki directory and retrieve relevant information
+        File deckdir = new File("wikis/");
+        deckdir.mkdir();
+        File[] files = deckdir.listFiles();
+        for (File f : files) {
+            if (f.isFile() && f.getName().endsWith(".html")) {
+                FileReader reader;
+                try {
+                    reader = new FileReader("wikis\\" + f.getName());
+                } catch (FileNotFoundException e) {
+                    System.out.println("Unable to recognize file name.");
+                    return;
+                }
+                Scanner in = new Scanner(reader);
+                while (in.hasNext()) {
+                    try {
+                        while (in.hasNext()) {
+                            String title = null;//write method to find title
+                            String description = null;//write method to find description
+                            String location = null;//write method to find location
+                            LocalDate date = null;//write method to find date
+                            Wiki w = new Wiki(title, description, location, date);
+                            wikis.add(w);
+                            try {
+                                in.nextLine();
+                            } catch (Exception ignore) {
+                            }
+                        }
+                    } catch (InputMismatchException e) {
+                        in.next();
+                    }
+                }
+            }
+        }
     }
 }
